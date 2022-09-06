6c0889d8011e01b01cad907ee0b8a312745b6e52
Mohamed Hesham <moohisham2000@gmail.com>
2022-09-06 19:25:56 +0200

    feat
    active classes to nav

--- components/Layout/MainNavigation.js
+++ components/Layout/MainNavigation.js
@@ -1,7 +1,10 @@
 import Logo from "./Logo";
 import Link from "next/link";
 import styles from "./MainNavigation.module.css";
+import { useRouter } from "next/router";
 const MainNavigation = () => {
+  const router = useRouter();
+
   return (
     <header className={styles.header}>
       <Link href="/">
@@ -12,10 +15,18 @@ const MainNavigation = () => {
       <nav>
         <ul>
           <li>
-            <Link href="/posts">Posts</Link>
+            <Link href="/posts">
+              <a className={router.pathname == "/posts" ? styles.active : ""}>
+                Posts
+              </a>
+            </Link>
           </li>
           <li>
-            <Link href="/contact">Contact</Link>
+            <Link href="/contact">
+              <a className={router.pathname == "/contact" ? styles.active : ""}>
+                Contact
+              </a>
+            </Link>
           </li>
         </ul>
       </nav>
