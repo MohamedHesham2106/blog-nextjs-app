8278a8866ae4cf75e394c3ff7a5afe4421519cae
Mohamed Hesham <moohisham2000@gmail.com>
2022-09-06 21:16:47 +0200

    feat
    configuration

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_cluster_name}.uggmscg.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+const connectionString = process.env.server;
 
 export const connectDB = async () => {
   const client = await MongoClient.connect(connectionString);
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
-    // add your configuration 
+    // add your configuration
     return {
       env: {
         mongodb_username: "",
@@ -15,10 +15,7 @@ module.exports = (phase) => {
   }
   return {
     env: {
-      mongodb_username: process.env.DB_USERNAME,
-      mongodb_password: process.env.DB_PASSWORD,
-      mongodb_cluster_name: process.env.CLUSTER,
-      mongodb_database: process.env.DATABASE,
+      server: process.env.SERVER,
     },
   };
 };
